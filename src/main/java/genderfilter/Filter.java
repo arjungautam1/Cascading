2f4ac85b8aabbe11f3812f4223b22ce7152f9814
arjungautam1 <arjungautam5431@gmail.com>
2021-09-20 11:38:18 +0545

    reformat and optimize imports

--- src/main/java/genderfilter/Filter.java
+++ src/main/java/genderfilter/Filter.java
@@ -2,7 +2,6 @@ package genderfilter;
 
 import cascading.flow.Flow;
 import cascading.flow.FlowDef;
-import cascading.flow.local.LocalFlow;
 import cascading.flow.local.LocalFlowConnector;
 import cascading.pipe.Each;
 import cascading.pipe.Pipe;
@@ -20,30 +19,21 @@ public class Filter {
         String sinkPath = "src/main/resources/filter_sink.txt";
 
         Tap sourceTap = new FileTap(new TextDelimited(true, ";"), sourcePath);
-        Tap sinkTap=new FileTap(new TextDelimited(true,"|"),sinkPath, SinkMode.REPLACE);
+        Tap sinkTap = new FileTap(new TextDelimited(true, "|"), sinkPath, SinkMode.REPLACE);
 
 //        3.Now create a pipe on which above operation will be performed
 
-        Pipe pipe=new Pipe("genderFilterPipe");
-        pipe=new Each(pipe,new GenderFilter());
+        Pipe pipe = new Pipe("genderFilterPipe");
+        pipe = new Each(pipe, new GenderFilter());
 
 //        4.Finally, we create flow and execute the operation
-        FlowDef flowDef=FlowDef
+        FlowDef flowDef = FlowDef
                 .flowDef()
-                .addSource(pipe,sourceTap)
-                .addTailSink(pipe,sinkTap);
+                .addSource(pipe, sourceTap)
+                .addTailSink(pipe, sinkTap);
 
-        Flow flow=new LocalFlowConnector().connect(flowDef);
+        Flow flow = new LocalFlowConnector().connect(flowDef);
         flow.complete();
-
-
-
-
-
-
-
-
-
 
 
     }
